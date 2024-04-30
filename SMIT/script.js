b3ae65df33754485f55c2bef0711ac58b62d60b0
siddiqui2202 <153586242+siddiqui2202@users.noreply.github.com>
2024-04-30 14:37:13 +0500

    Javascript project

---
+++ SMIT/script.js
@@ -0,0 +1,116 @@
+var user_number = document.getElementById("user_number");
+var random_number = document.getElementById("random_number");
+var result = document.getElementById("result");
+var info_box_result = document.getElementById("info_box_result");
+var win_html = document.getElementById("win");
+var loss_html = document.getElementById("loss");
+var total = document.getElementById("total");
+var msg = document.getElementById("msg");
+
+var count = 10;
+var win = 0;
+var loss = 0;
+
+function tryMyLuck(userNumber) {
+  // alert(userNumber);
+  var randomNumber = Math.random() * 6;
+  var ceil = Math.ceil(randomNumber);
+  console.log(ceil);
+  user_number.innerText = userNumber;
+  random_number.innerText = ceil;
+
+  // total count mein se aik minus krdo
+  total.innerText = --count;
+  
+  if (ceil === userNumber) {
+    console.log("user wins");
+    result.innerText = "You won";
+    info_box_result.style.backgroundColor = "Green";
+    result.style.color = "White";
+    // total count mein se aik minus krdo
+    win_html.innerText = ++win;
+  } else {
+    result.innerText = "You Loss";
+    info_box_result.style.backgroundColor = "Red";
+    result.style.color = "White";
+    loss_html.innerText = ++loss;
+  }
+
+  checkResult();
+}
+
+function checkResult() {
+  if (count == 0) {
+    if (win >= 3) {
+      msg.innerText = "Congrats! You won";
+    } else {
+      msg.innerText = "Sorry! You loss try again";
+    }
+    count = 10;
+    win = 0;
+    loss = 0;
+    total.innerText = count;
+    win_html.innerText = loss;
+    loss_html.innerText = win;
+  }
+}
+
+// Rolling Dice option
+
+const dice = document.querySelector('.dice');
+const rollBtn = document.querySelector('.roll');
+
+const randomDice = () => {
+
+    const random = Math.floor(Math.random() * 10);
+
+    if (random >= 1 && random <= 6) {
+        rollDice(random);
+    }
+    else {
+        randomDice();
+    }
+}
+
+const rollDice = random => {
+
+    dice.style.animation = 'rolling 4s';
+
+    setTimeout(() => {
+
+        switch (random) {
+            case 1:
+                dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                break;
+
+            case 6:
+                dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                break;
+
+            case 2:
+                dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+                break;
+
+            case 5:
+                dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                break;
+
+            case 3:
+                dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+                break;
+
+            case 4:
+                dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+                break;
+
+            default:
+                break;
+        }
+
+        dice.style.animation = 'none';
+
+    }, 4050);
+
+}
+
+rollBtn.addEventListener('click', randomDice);
